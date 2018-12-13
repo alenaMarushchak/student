73876a25b11889b3a110e064df3a627066fd783b
marushchak <maruschak68@gmail.com>
2018-12-13 11:25:44 +0200

    last fix

--- helpers/validator/schemas/user/index.js
+++ helpers/validator/schemas/user/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const USER_SIGN_IN = require('./signIn');
-const USER_CHANGE_PASSWORD = require('./changePassword');
 const USER_CREATE = require('./createUser');
 const UPDATE_USER = require('./updateUser');
 
 module.exports = {
     USER_SIGN_IN,
-    USER_CHANGE_PASSWORD,
     USER_CREATE,
     UPDATE_USER
 };
